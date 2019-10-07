fe44aef844107fb56466a5c3b636f7a27df261b8
Andrey <andreyka.kolesnik.00@mail.ru>
2019-10-08 01:06:33 +0300

    start

---
+++ laba3/ConsoleApp1/ConsoleApp1/Phone.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    class Phone
+    {
+        public readonly int ID;
+        public string Name;
+        public string SurName;
+        public string SecondName;
+        private string Adress;
+        private string Number;
+        private double Debet;
+        private double Credit;
+        private string TimeCity;
+        private string TimeLongDistance;
+        public Phone()
+        {
+
+        }
+    }
+}
