bfbf7780d2ff3df965e194515a9d1379e21e76a9
Andrey <andreyka.kolesnik.00@mail.ru>
2019-10-08 04:12:34 +0300

    lab3

--- laba3/ConsoleApp1/ConsoleApp1/Phone.cs
+++ laba3/ConsoleApp1/ConsoleApp1/Phone.cs
@@ -1,20 +1,80 @@
-﻿namespace ConsoleApp1
+﻿using System;
+namespace ConsoleApp1
 {
-    class Phone
+    partial class Phone
     {
-        public readonly int ID;
-        public string Name;
-        public string SurName;
-        public string SecondName;
-        private string Adress;
-        private string Number;
-        private double Debet;
-        private double Credit;
-        private string TimeCity;
-        private string TimeLongDistance;
+        static public readonly string ID = "qwe";
+        public const string UNIVER = "BSTU";
+        static public int count;
+        private int number;
+        private string Name;
+        private string Adress { get; set; }
+        
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                if(Number > 0)
+                {
+                    number = value;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректные данные");
+                }
+            }
+        }
+        private string TimeCity { get; set; }
+        private string TimeLongDistance { get; set; }
+
         public Phone()
         {
+            Console.WriteLine("Конструктор по умолчанию");
+            count++;
 
+        }
+
+        public Phone(string name, string number)
+        {
+            Name = name;
+            Number = Convert.ToInt32(number);
+            Console.WriteLine($"{Name} с номером {Number}");
+            count++;
+
+        }
+
+        public Phone(string name)
+        {
+            Name = name;
+            Console.WriteLine($"модель телефона {Name}");
+            count++;
+
+        }
+
+        static Phone()
+        {
+
+            Console.WriteLine("Вызывлся статиический конструктор");
+            Console.WriteLine(ID.GetHashCode());
+        }
+
+        private Phone(int a)
+        {
+
+        }
+
+        static public void Sum(ref int a, out int b)
+        {
+            b = a + a;
+        }
+
+        static public void Info()
+        {
+            Console.WriteLine($"Объектов создано {count}");
         }
     }
 }
--- laba3/ConsoleApp1/ConsoleApp1/Program.cs
+++ laba3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,21 @@
-﻿
+﻿using System;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Phone Android = new Phone();
+            Phone Android2 = new Phone();
+            Phone Android3 = new Phone("kek", "12341234");
+            Console.WriteLine(Android3);
+            Phone.Info();
+
+            int x = 5;
+            int y;
+            Phone.Sum(ref x, out y);
+            Console.WriteLine($"x = {x} y = {y}");
+
 
         }
     }
