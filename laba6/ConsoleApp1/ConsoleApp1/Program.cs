d735195e5ed185d45269178aa9bd46416fa218e3
Andrey <andreyka.kolesnik.00@mail.ru>
2019-10-22 18:59:02 +0300

    6 laba

---
+++ laba6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producer Kail = new Producer();
+            Person person = new Person();
+            Person persona = Kail;
+            if (person is Producer)
+            {
+                Producer Igor = (Producer)person;
+            }
+            else
+            {
+                Console.WriteLine("Преобразовать нельзя");
+            }
+            Producer Lip = person as Producer;
+            if (Lip == null)
+            {
+                Console.WriteLine("Преобразование прошло неудачно");
+            }
+            else
+            {
+                Console.WriteLine("Преобразование удалось");
+            }
+
+
+            Kail.name = "Kail";
+
+            Kail.Move();
+            Console.WriteLine(Kail.ToString());
+            Console.WriteLine(persona.name);
+            Film Transformers = new Film(2005);
+            Film Hello = new Film(2003);
+            Transformers.time = 245;
+            Cartoon Naruto = new Cartoon(6);
+            News GC = new News(21);
+            News LL = new News(18);
+            Printer Print = new Printer();
+            Transformers.Watch();
+            Naruto.Watch();
+            GC.Watch();
+            ProgramGuide Hai = new ProgramGuide();
+            Hai.Year = 2015;
+            ProgramGuide Hai2 = new ProgramGuide();
+            Hai.Year = 2003;
+            ProgramGuide Hai3 = new ProgramGuide();
+            Hai.Year = 2015;
+            ProgramGuide Hai4 = new ProgramGuide(200);
+            Hai.Year = 2007;
+            
+
+
+            List<object> array = new List<object>() { Kail, person, Naruto, GC, Transformers, Print };
+            foreach (object ch in array)
+            {
+                Print.IAmPrinting(ch);
+            }
+            List<ProgramGuide> film = new List<ProgramGuide>() { Hai, Hai2, Hai3 };
+            film.Add(Hai4);
+            Controller.Count();
+            Controller.Search(film);
+            Hai4.Inf();
+        }
+        User Andrey = new User("Andrey", 19);
+        
+        obj Fr;
+       
+        
+        
+        
+        
+        
+    
+        
+        
+    }
+}
---
+++ laba6/ConsoleApp1/ConsoleApp1/TVprogram.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person : IPerson
+    {
+        public string name;
+        public int age;
+        public virtual void Move()
+        {
+            Console.WriteLine("Человек идет");
+        }
+
+        public override string ToString()
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                return "Имя не определено";
+            }
+            return "Продюссера зовут " + name;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+
+    interface ITVprogram
+    {
+        void Watch();
+    }
+
+    interface IPerson
+    {
+        void Move();
+    }
+
+    class TVprogram 
+    {
+        public int agelimit;
+        public double time;
+    }
+
+    class Film : TVprogram, ITVprogram
+    {
+        private int year;
+        static int limit = 16;
+        
+       
+        public Film(int ch)
+        {
+            year = ch;
+        }
+        public void Watch()
+        {
+            if (agelimit > limit)
+            {
+                Console.WriteLine("Фильм начнется через 20 мин");
+            }
+            else
+            {
+                Console.WriteLine("Вам рано еще смотреть этот фильм");
+            }
+        }
+        public override string ToString()
+        {
+            return $"Возрастное ограничение на просмотр этого фильма {agelimit}";
+        }
+    }
+
+    class News : TVprogram, ITVprogram
+    {
+        public static int count = 0;
+        static int limit = 18;
+        public News(int age)
+        {
+            agelimit = age;
+            count++;
+        }
+        public override string ToString()
+        {
+            return $"Возрастное ограничение этих новостей {limit}";
+        }
+        public void Watch()
+        {
+            if (agelimit > limit)
+            {
+                Console.WriteLine("Новости начнутся через 20 мин");
+            }
+            else
+            {
+                Console.WriteLine("Вам рано смотреть");
+            }
+        }
+    }
+
+    class Cartoon : TVprogram, ITVprogram
+    {
+        static int limit = 8;
+        public Cartoon(int age)
+        {
+            agelimit = age;
+        }
+        public override string ToString()
+        {
+            return $"Возрастное ограничение этого мультфильма {limit}";
+        }
+        public void Watch()
+        {
+            if (agelimit > limit)
+            {
+                Console.WriteLine("Мультфильм начнется через 20 мин");
+            }
+            else
+            {
+                Console.WriteLine("Вам нельзя это смотреть");
+            }
+        }
+    }
+
+    sealed class Producer : Person
+    {
+        public string education;
+        public override void Move()
+        {
+            Console.WriteLine($"{name} идет");
+        }
+
+    }
+
+
+    public class Printer
+    {
+        public void IAmPrinting(object obj)
+        {
+            obj.GetType();
+            Console.WriteLine(obj.ToString());
+        }
+    }
+
+
+    /// <summary>
+    /// 6 laba
+    /// </summary>
+
+
+    
+    public class ProgramGuide
+    {
+        private int yer;
+        public int vremya;
+        public ProgramGuide()
+        {
+
+        }
+        public ProgramGuide(int vr)
+        {
+            vremya = vr;
+        }
+        public void Inf()
+        {
+            Console.WriteLine($"Продолжительность передачи {vremya + time}");
+        }
+        public static int time = 20;
+        public int Year
+        {
+            get
+            {
+                return yer;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    yer = value;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректные данные");
+                }
+            }
+        }
+        public static int year = 2015;
+        internal List<ProgramGuide> Spis = new List<ProgramGuide>();
+        void Add(ProgramGuide c)
+        {
+            Spis.Add(c);
+            Console.WriteLine($"В список добавлен {c}");
+        }
+        void Remove(ProgramGuide c)
+        {
+            Spis.Remove(c);
+            Console.WriteLine($"Из списка удален {c}");
+        }
+        void Output(List<ProgramGuide> c)
+        {
+            foreach(object ch in c)
+            {
+                Console.WriteLine(ch);
+            }
+        }
+        
+        
+        
+    }
+    class Controller
+    {
+        List<object> l = new List<object>();
+        
+       public static void Count()
+        {
+            Console.WriteLine($"Число рекламных роликов равно {News.count}");
+        }
+        public static void Search(List<ProgramGuide> spis)
+        {
+            for(int i=0;i<spis.Count; i++)
+            {
+                if(spis[i].Year == ProgramGuide.year)
+                {
+                    Console.WriteLine($"{spis[i]} снимался в 2015 году");
+                }
+                else
+                {
+                    Console.WriteLine($"{spis[i]} не снимался в 2015 году");
+                }
+            }
+        }
+    }
+    struct User
+    {
+        public string Name;
+        public int Age;
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Имя юзера {Name}, его возраст {Age}");
+        }
+    }
+
+    enum obj
+    {
+        Monday = 1,
+        Tuesday,
+        Whensday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
